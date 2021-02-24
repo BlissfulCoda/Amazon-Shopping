f5b08cb7a35f1cb8525e11607d09d328a9b442ac
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-02-24 18:23:45 +0000

    inital setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const user = {
+    name: 'Ronnie',
+    active: true,
+    cart: [],
+    purchases: []
+}
+
+function purchaseItem(){}
+
+function addItemToCart(){}
+
+function applyTaxToItems(){}
+
+function buyItem() {}
+
+function emptyCart() {}
